6365e340f16d9e34a6116670dd1b027baead4dc9
PAUL DIMITRIS KYRIOU <paulkyriou@PAULs-MacBook-Pro.local>
2023-05-19 13:47:40 +0100

    second test passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,5 +1,17 @@
 # WRITE YOUR FUNCTIONS HERE
+
 def get_pet_shop_name(pet_shop):
     # return "Camelot of Pets"
     return pet_shop["name"]
+
+# def add_or_remove(pet_shop,cash):
+    #pbd.set_trace()
+    # pet_shop["admin"]["total_cash"] += cash
+
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
+
+
+
+
 
