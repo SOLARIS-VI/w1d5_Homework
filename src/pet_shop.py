21baedb0a56c83c610a8180a60f363da59e57b81
PAUL DIMITRIS KYRIOU <paulkyriou@PAULs-MacBook-Pro.local>
2023-05-19 16:01:41 +0100

    fifth test passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -13,7 +13,12 @@ def add_or_remove_cash(pet_shop, amount):
 def test_add_or_remove_cash_add(pet_shop, total_cash):
     pet_shop["admin"]["total_cash"] += 10
     pet_shop["admin"]["total_cash"] -= -10
-    
+
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
+
+
 
 
 
