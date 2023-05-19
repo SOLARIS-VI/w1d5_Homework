961fe1169e4039bf4bd2b43bc156781266ca0d75
PAUL DIMITRIS KYRIOU <paulkyriou@PAULs-MacBook-Pro.local>
2023-05-19 15:24:21 +0100

    fourth test passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -4,15 +4,19 @@ def get_pet_shop_name(pet_shop):
     # return "Camelot of Pets"
     return pet_shop["name"]
 
-# def add_or_remove(pet_shop,cash):
-    #pbd.set_trace()
-    # pet_shop["admin"]["total_cash"] += cash
-
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
 
 def add_or_remove_cash(pet_shop, amount):
     pet_shop["admin"]["total_cash"] += amount
+
+def test_add_or_remove_cash_add(pet_shop, total_cash):
+    pet_shop["admin"]["total_cash"] += 10
+    pet_shop["admin"]["total_cash"] -= -10
+    
+
+
+
 
 
 
