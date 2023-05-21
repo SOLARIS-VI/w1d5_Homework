dc945fa5a97308e5fdfdbe8169b1f045c0421ac0
PAUL DIMITRIS KYRIOU <paulkyriou@PAULs-MacBook-Pro.local>
2023-05-21 22:02:17 +0100

    tenth and eleventh test passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,26 +1,32 @@
 # WRITE YOUR FUNCTIONS HERE
 
+
 # FIRST PASS TEST
 def get_pet_shop_name(pet_shop):
     # return "Camelot of Pets"
     return pet_shop["name"]
 
+
 # SECOND PASS TEST
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
 
+
 # THIRD PASS TEST
 def add_or_remove_cash(pet_shop, amount):
     pet_shop["admin"]["total_cash"] += amount
+
 
 # FOURTH PASS TEST
 def test_add_or_remove_cash_add(pet_shop, total_cash):
     pet_shop["admin"]["total_cash"] += 10
     pet_shop["admin"]["total_cash"] -= -10
 
+
 # FIFTH PASS TEST
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
+
 
 # SIXTH PASS TEST
 def increase_pets_sold(pet_shop, sold):
@@ -31,6 +37,7 @@ def increase_pets_sold(pet_shop, sold):
 def get_stock_count(pet_shop):
     return len(pet_shop["pets"])
 
+
 # EIGHTH & NINTH PASS TEST
 def get_pets_by_breed(pet_shop, pet_name):
     pets = []
@@ -39,8 +46,17 @@ def get_pets_by_breed(pet_shop, pet_name):
             pets.append(pet)
 
     return pets
-    
-    
+
+
+# TENTH & ELEVENTH PASS TEST
+def find_pet_by_name(pet_shop, pet_name):
+    pets = pet_shop["pets"]
+    for pet in pets:
+        if pet["name"] == pet_name:
+            return pet
+    return None
+
+
 
 
 
