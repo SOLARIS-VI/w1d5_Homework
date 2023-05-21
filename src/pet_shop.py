d808c77736a83238f26f01bbb32e2a690ce90802
PAUL DIMITRIS KYRIOU <paulkyriou@PAULs-MacBook-Pro.local>
2023-05-21 22:44:23 +0100

    twelth test passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -57,6 +57,14 @@ def find_pet_by_name(pet_shop, pet_name):
     return None
 
 
+# TWELTH PASS TEST
+def remove_pet_by_name(pet_shop, pet_name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == pet_name:
+            pet_shop["pets"].remove(pet)
+
+
+
 
 
 
